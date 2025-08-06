da83e4faf22e3ad7794d71f43303a810cee7f178
7777hts-prog <7777hts@gmail.com>
2025-08-06 18:23:43 +0900

    Update main.py
    from flask import Flask
    
    app = Flask(__name__)
    
    @app.route('/')
    def home():
        return 'Bot is running!'
    
    if __name__ == '__main__':
        print("월하 봇 시작!")
        app.run(host='0.0.0.0', port=10000)

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("월하 봇 시작!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Bot is running!'
+
+if __name__ == '__main__':
+    print("월하 봇 시작!")
+    app.run(host='0.0.0.0', port=10000)
